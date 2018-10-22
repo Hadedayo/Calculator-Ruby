0650b4771597fca6ae3be30c3e95ec992a1390a8
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-10-22 23:05:33 +0100

    Added the bonus - BMI Task

--- index.rb
+++ index.rb
@@ -62,3 +62,43 @@ end
 if (interest == "sqrt")
   puts sqrt number1
 end
+
+#           BMI
+def bmi_imperial weight,height
+  return (weight * 703)/(height * height)
+end
+
+def bmi_metric weight,height
+  return (weight)/(height * height)
+end
+
+
+
+puts "Would you like to calculate your BMI?"
+bmi = gets.chomp()
+
+if (bmi == "yes")
+  puts "Let us proceed, I will need information about your weight and height"
+  puts "Choose what system you want to calculate your BMI in: imperial or metric?"
+  bmi_options = gets.chomp()
+  if (bmi_options == "imperial")
+    puts "What is your weight (lb)?"
+    weight = gets.chomp().to_i
+    puts "What is your height (in)?"
+    height = gets.chomp().to_i
+    puts bmi_imperial weight,height
+
+  elsif (bmi_options == "metric")
+    puts "What is your weight (kg)?"
+    weight = gets.chomp().to_i
+    puts "What is your height (m)?"
+    height = gets.chomp().to_i
+    puts bmi_metric weight,height
+  else
+    puts "That's all we have for now. Have a nice day and be sure to come back"
+  end
+
+
+else
+  puts "Have a nice day! Be sure to come back"
+end
