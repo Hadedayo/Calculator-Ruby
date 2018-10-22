7c7d64db7d36182529a89649a0f36f9628973624
Hassanat Adedayo <hadedayo@spartaglobal.com>
2018-10-22 22:31:18 +0100

    Added advanced functions-sqrt and power

--- index.rb
+++ index.rb
@@ -1,27 +1,39 @@
+def add_two_numbers number1, number2
+  return number1 + number2
+end
+
+def subtract_two_numbers number1, number2
+  return number1 - number2
+end
+
+def multiply_two_numbers number1, number2
+  return number1 * number2
+end
+
+def divide_two_numbers number1, number2
+  return number1 / number2
+end
+
+def power number1, number2
+  return number1 ** number2
+end
+
+def sqrt number1
+  return Math.sqrt(number1)
+end
+
 puts "Welcome to my calculator"
 puts "What is your interest, R or A?"
 $stdout.flush
 choice = gets.chomp()
-puts choice
+puts choice + " calulations below"
 
-def add_two_numbers number1, number2
-  return number1 + number2
-end
-def subtract_two_numbers number1, number2
-  return number1 - number2
-end
-def multiply_two_numbers number1, number2
-  return number1 * number2
-end
-def divide_two_numbers number1, number2
-  return number1 / number2
-end
 
 puts "Enter a Number"
 number1 = gets.chomp().to_i
 puts "Enter another number"
 number2 = gets.chomp().to_i
-puts "What is your interest: add, subtract, multiply, divide, power & square root?"
+puts "What is your interest: add, subtract, multiply, divide, power & sqrt?"
 interest = gets.chomp()
 
 #               REGULAR
@@ -43,11 +55,10 @@ end
 
 
 #               ADVANCED
-
-def power number1, number2
-  return number1 ** number2
+if (interest == "power")
+  puts power number1, number2
 end
 
-if (interest == power)
-  puts power number1, number2
+if (interest == "sqrt")
+  puts sqrt number1
 end
